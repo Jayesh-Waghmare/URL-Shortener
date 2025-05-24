cf1222b2120e10c694d17b58417eba7582be2180
Jayesh Waghmare <waghmarejayesh751@gmail.com>
2025-05-24 18:29:41 +0530

    commit

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -16,9 +16,12 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
+
+// Optional but helpful for OPTIONS preflight
+app.options("*", cors());
 
 
 app.use(express.json())
