16bf65915ad0f6f8d8b7aae0713f6b0885f4eee6
Jayesh Waghmare <waghmarejayesh751@gmail.com>
2025-05-25 10:48:35 +0530

    fix changes

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -2,7 +2,7 @@ export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "None",
-    maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    maxAge: 1000 * 60 * 60 * 10, // 10 minutes
     path: "/",
     domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : undefined
 }
